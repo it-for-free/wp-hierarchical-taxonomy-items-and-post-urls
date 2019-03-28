31d6bd9a6c400dbf777a1a3abc8e3b807a2dffdb
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-03-28 15:59:36 +0300

    Переход на ООП формат описания решения.

--- wp-hierarchical-taxonomy-items-and-post-urls.php
+++ wp-hierarchical-taxonomy-items-and-post-urls.php
@@ -10,7 +10,8 @@
 
 use ItForFree\WpAddons\Modules\HierarhicalUrls\HierarhicalUrls;
 
-//(new HierarhicalUrls())->init('uslugicat');
+use ItForFree\WpAddons\Core\Admin\Settings\SettingsPage\SettingsPage;
+use ItForFree\WpAddons\Core\Admin\Settings\SettingsPage\Section\Field\Html\TaxonomiesCheckboxList;
 
 $options = get_option('htpu_options');
 
@@ -20,61 +21,16 @@ if (!empty($options['checked_taxonomies'])) {
     }
 }
 
+$Page = new SettingsPage('htpu', 
+    "Плагин иерархических URL для элементов таксономии и связанных с ними записей",
+    "Плагин иерархических URL");
+$Page->createAndAddSettingsEntity()
+    ->createAndAddSection('main', 'Настройки плагина', 'Используйте форму ниже, чтобы задать настройки. <br>'
+            . 'ВНИМАНИЕ: работа плагина требует, чтобы тип контента с тем же самым slug базовым был зарегистрирован раньше, чем таксономия '
+            . '(например. может потребовать правка в модуле CPT UI). ');
 
-// Добавляем пункт подменю для настроек плагнина в Settings menu
-add_action('admin_menu', 'htpu_options_menu');
+$Page->getSectionById('main')->addSectionField(
+   new TaxonomiesCheckboxList($Page->getSectionById('main'), 'checked_taxonomies',
+        'Выбирите типы такосономий, дя которых следует активировать плагин')
+);
 
-function htpu_options_menu() {
-    add_options_page('Module Hierarchical Taxonomy Nested Items and Post urls', 'Hierarchical Taxonomy URLs', 'manage_options', 'htpu-options', 'htpu_options');
-    add_action('admin_init', 'htpu_register_settings');
-}
-
-// Регистрируем настройки плагина
-function htpu_register_settings() {
-    register_setting('htpu_options', 'htpu_options', 'htpu_options_validate');
-
-    add_settings_section('htpu_settings', 'Используемые таксономии', 'htpu_section_text', 'htpu-options');
-    add_settings_field('htpu_checked_taxonomies', 'Включить плагин для таксономий:', 'htpu_checked_taxonomies', 'htpu-options', 'htpu_settings');
-}
-
-// Вывод чекбоксов для таксономий
-function htpu_checked_taxonomies() {
-    $options = get_option('htpu_options');
-
-    $disabled_taxonomies = array('nav_menu', 'link_category', 'post_format');
-    foreach (get_taxonomies() as $tax) : if (in_array($tax, $disabled_taxonomies))
-            continue;
-        ?>
-        <input type="checkbox" name="htpu_options[checked_taxonomies][<?php echo $tax ?>]" value="<?php echo $tax ?>" <?php checked(isset($options['checked_taxonomies'][$tax])); ?> /> <?php echo $tax; ?><br />
-    <?php
-    endforeach;
-}
-
-// Вывод описания раздела
-function htpu_section_text() {
-    echo '<p>Выберете таксономии, '
-    . 'для категориий которых (и записях, относящихся к этим категориям),'
-    . ' будет включен данный плагин иерархических ссылок.</p>';
-}
-
-// Валидация настроек
-function htpu_options_validate($input) {
-    return $input;
-}
-
-function htpu_options() {
-    if (!current_user_can('manage_options'))
-        wp_die('You do not have sufficient permissions to access this page.');
-    $options = get_option('htpu_options');
-    ?>
-    <div class="wrap">
-
-        <h2><?php echo 'Hierarchical URLs'; ?></h2>
-        <form method="post" action="options.php">
-            <?php settings_fields('htpu_options'); ?>
-            <?php do_settings_sections('htpu-options'); ?>
-            <?php submit_button(); ?>
-        </form>
-    </div>
-    <?php
-}
